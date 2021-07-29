9895d7a079a39670053f0081d763c69689881313
omidmaldar <maldar@gmail.com>
2021-07-29 10:24:02 +0300

    Fix issues in dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,24 +3,24 @@ import sbt._
 object Versions {
   val AkkaHttp = "10.2.5"
   val AkkaStream = "2.6.8"
-  val AkkaHttpJSON = " 1.36.0"
+  val AkkaHttpJSON4s = "1.36.0"
   val JSON4s = "4.0.3"
   val ScalaLogging = "3.9.4"
-  val Slf4j = "1.7.31"
+  val Slf4j = "1.7.32"
 }
 
 object Dependencies {
   
   private val akkaHttp = Seq(
-    "com.typesafe.akk" %% "akka-http" % Versions.AkkaHttp
+    "com.typesafe.akka" %% "akka-http" % Versions.AkkaHttp
   )
 
   private val akkStream = Seq(
-    "com.typesafe.akka" %% "akka-http" % Versions.AkkaStream
+    "com.typesafe.akka" %% "akka-stream" % Versions.AkkaStream
   )
 
   private val akkaHttpJSON = Seq(
-    "com.typesafe.akka" %% "akka-http-json4s" % Versions.AkkaHttpJSON
+    "de.heikoseeberger" %% "akka-http-json4s" % Versions.AkkaHttpJSON4s
   )
 
   private val json4s = Seq(
@@ -28,11 +28,11 @@ object Dependencies {
   )
 
   private val scalaLogging = Seq(
-    "com.typesafe.scala-logging" %% " scala-logging" % Versions.ScalaLogging
+    "com.typesafe.scala-logging" %% "scala-logging" % Versions.ScalaLogging
   )
 
   private val slf4j = Seq(
-    "org.slf4j" %% "slfj-simple" % Versions.Slf4j
+    "org.slf4j" % "slf4j-simple" % Versions.Slf4j % Test
   )
   
 
